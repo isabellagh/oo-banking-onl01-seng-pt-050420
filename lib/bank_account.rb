fb40d2d4dcd4a6aeca3c7536d6629b48e2a7165d
isabella g henriques <isabellagh88@gmail.com>
2020-06-04 21:06:27 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,26 +3,29 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
-  def initialize(name)
-    @name = name 
-    @balance = 1000
-    @status = "open"
-  end 
-  
-  def deposit(money)
-    @balance += money
-  end 
-  
-  def display_balance
-    "Your balance is $#{@balance}."
-  end 
-  
-  def valid?
-     @status == "open" && @balance > 0 ? true : false
-  end 
-  
-  def close_account
-    @status = "closed"
-  end 
+  def initialize(name, balance=1000, status="open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
 
-end
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def withdrawal(amount)
+    self.balance -= amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0 ? true : false
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+end 
