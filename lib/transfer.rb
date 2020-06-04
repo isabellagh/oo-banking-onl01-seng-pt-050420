94da9dc4c949aa2fe0ce5036d3b96f3f846d68d8
isabella g henriques <isabellagh88@gmail.com>
2020-06-04 21:01:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,14 +15,14 @@ class Transfer
   end 
   
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.withdrawal(self.amount)
+      receiver.deposit(self.amount)
+      self.status = "complete"
     else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end 
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
   end 
   
   def reverse_transfer
