7b758967f0c752c59c3a8ea7b9520e34f0be4298
isabella g henriques <isabellagh88@gmail.com>
2020-06-04 20:50:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
+      @status = "complete"
   end 
   
 end
