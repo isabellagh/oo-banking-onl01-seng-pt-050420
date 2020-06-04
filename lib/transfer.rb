2f6422ea1a19f2defa39b94eed11e4f8690bc663
isabella g henriques <isabellagh88@gmail.com>
2020-06-04 21:05:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,36 +1,36 @@
 class Transfer
   
-  attr_accessor :sender, :receiver, :status, :amount
-  
-  
-  def initialize(sender, receiver, amount)
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount, status="pending")
     @sender = sender
     @receiver = receiver
-    @status = "pending"
     @amount = amount
-  end 
-  
+    @status = status
+  end
+
   def valid?
-    @sender.valid? && @receiver.valid? ? true : false
-  end 
-  
+    sender.valid? && receiver.valid?
+  end
+
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.withdrawal(self.amount)
+      receiver.deposit(self.amount)
+      self.status = "complete"
     else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end 
-  end 
-  
-  def reverse_transfer
-    if @status == "complete"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
-      @status = "reversed"
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
-  
-end
+
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      sender.deposit(self.amount)
+      receiver.withdrawal(self.amount)
+      self.status = "reversed"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."      
+    end
+  end
